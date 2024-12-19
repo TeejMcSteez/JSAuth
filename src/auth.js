19b27a6681601877b781016bf8e5a4ceaf409a4f
TeejMcSteez <tjhall047@gmail.com>
2024-12-19 15:42:07 -0500

    Decided to use mongodb instead of SQL as I dont want to deal with the rigidity of SQL and mongodb looks cool

--- src/auth.js
+++ src/auth.js
@@ -1,42 +1,23 @@
-const { Auth } = require('pg');
+// 
 const bcrypt = require('bcrypt');
-
-const authClient = new Auth({
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME
-});
-
-await authClient.connect();
-
-async function AuthUser(name, password) {
-    try {
-        const query = {
-            text: `SELECT password FROM users WHERE username = $1`,
-            values: [name],
-        };
-    
-        const res = await authClient.query(query);
-    
-        if (res.rows.length === 0) {
-            return {success: false, message: 'User not found'};
-        }
-    
-        const hashedPasswordFromDB = res.rows[0].password;
-        const isMatch = await bcrypt.compare(password, hashedPasswordFromDB);
-    
-        if (isMatch) {
-            return {success: true, message: 'Auth Success'};
-        } else {
-            return {success: false, message: 'Invalid Password'};
-        }
-    } catch (error) {
-        console.error(`Error during auth: ${error.message}`);
-        return {success: false, message: 'An error occured during auth'};
+const db = require('mongodb');
+const jwt = require('jsonwebtoken');
+const env = require('dotenv');
+// Need to figure out how to properly find the mongodb endpoint
+class AuthService {
+    constructor(dbUri, dbName, jwtSecret) {
+        this.dbUri = dbUri;
+        this.dbName = dbName;
+        this.jwtSecret = jwtSecret;
+        this.client = new db(this.dbUri);
+        this.db = null;
     }
+    // Once the db creds have been found I need to connect to the db 
+    async connect() {
 
+    }
+    // Once connected to the db will check if user can be auth or not
+    async auth(username, password) {
+
+    }
 }
-
-module.exports = {AuthUser};
